a9b2f322dc7522057afb92e8e0596fbc8a523a57
AlessandroOrame <s315025@studenti.polito.it>
2024-09-11 15:17:40 +0200

    code riorganization in BrowninanMotionForHedging.py

--- brownianMotionForHedging.py
+++ brownianMotionForHedging.py
@@ -8,47 +8,42 @@ import logging
 
 class BrownianMotionForHedging(StochModel):
 
-    def __init__(self, sim_setting, option_list, dt, mu, sigma, rho, rnd_state): #TODO: sim_setting è inutile 
+    def __init__(self, 
+                 sim_setting, 
+                 option_list, 
+                 dt, mu, 
+                 sigma, rho, 
+                 rnd_state): # sim_setting is not used
+        
         super().__init__(sim_setting)
         self.dt = dt 
-        '''self.estimate_from_data(
-            sim_setting["start"],
-            sim_setting["end"]
-            )'''
         self.n_options = len(option_list)
         self.option_list = option_list
         self.mu = mu
         self.sigma = sigma
         self.corr = rho 
         self.rnd_state = rnd_state
-    
-    '''
-    def estimate_from_data(self, start, end):
-        hist_prices = yf.download(
-            self.tickers,
-            start = start,
-            end = end
-        )['Adj Close']
-        log_returns = np.log(hist_prices / hist_prices.shift(1)).dropna()
-        self.mu = log_returns.mean().values
-        self.sigma = log_returns.std().values
-        self.corr = log_returns.corr().values'''
 
-    def simulate_one_time_step(self, n_children, parent_node, remaining_times): # trova i prezzi di ogni stock (e delle opzioni)
-                                                            # per un nodo genitore, per tutti i suoi nodi figli
-        
+
+    def simulate_one_time_step(self, n_children, parent_node, remaining_times): 
+        # find the values (prices) of each asset for each node 
+        # in the new step with the same parent node
         parent_stock_prices= parent_node[1:self.n_shares+1] 
         parent_cash_price = parent_node[0]
 
         arb = True
         counter = 0
+        # Simulate stock prices until no-arbitarge is found:
         while (arb == True) and (counter<100):
             counter += 1
+            #In simulations settings: 
+            # S(t+dt) = S(t) * exp(mu - 1/2*sigma**2) * dt + sigma * sqrt(dt) * Z
+            # where Z is a standard normal distribution
             if self.n_shares > 1:
                 B = self.rnd_state.multivariate_normal(
                     mean = np.zeros(self.n_shares),
                     cov  = self.corr,
-                    size = n_children, #TODO: ricontrollare la dimensione (e confrontare con quello di Giovanni)
+                    size = n_children, # verify the correctness of the size
                     ).T    
             else: 
                 B = self.rnd_state.normal(loc = 0, scale = 1, size=n_children)
@@ -69,47 +64,19 @@ class BrownianMotionForHedging(StochModel):
         if counter >= 100:
             raise RuntimeError(f"No arbitrage solution NOT found after {counter} iteration(s)")
         else:
-            probs = 1/n_children * np.ones(n_children) # TODO probabilità uniformi??????
+            probs = 1/n_children * np.ones(n_children) # uniform probabilities ? 
         
-        # Options 
-        option_prices = np.zeros((self.n_shares, n_children))
+        # Options values
+        option_prices = np.zeros((self.n_options, n_children))
         for j in range(self.n_shares):
             S0 = stock_prices[j,:]
             time_to_maturity = remaining_times * self.dt 
-            # TODO: diamo per scontato che siano tutte opzioni europee, così da poter usare B&S
+            # hedging options are assumed to be of European type
             option_prices[j,:] = self.option_list[j].BlackScholesPrice(S0, time_to_maturity)
 
-
-        # Cash 
+        # Cash value
         cash_price = parent_cash_price * np.exp(self.option_list[0].risk_free_rate*self.dt) * np.ones(shape=n_children)
 
         prices = np.vstack((cash_price, stock_prices, option_prices))
-        #TODO: inserire nel nostro main
+        
         return probs, prices
-    
-
-    '''
-    def generate_states(self):
-        size = (self.branching_factor,)  #((self.n_underlyings, self.branching_factor))
-        if self.dynamics == 'BS':
-            if self.n_shares > 1:
-                B = random.multivariate_normal(
-                    mean = np.zeros(self.n_underlyings),
-                    cov  = self.rho,
-                    size = size,  #n_underlyings is automatic
-                    ).T
-            else: B = self.Obj.normal(loc = 0, scale = 1, size=size )
-            Y = np.array(self.sigma).reshape(-1,1) * np.sqrt(self.dt) * B
-            # c_rn   = self.r - 0.5 * self.sigma**2
-            c_hist = self.mu - 0.5 * self.sigma**2
-        if self.dynamics == 'VG':
-            G = self.Obj.gamma( shape = self.dt/self.nu, scale = self.nu, size=size ) # scale = 1 / rate
-            Y = self.Obj.normal( loc = self.mu*G, scale = self.sigma*np.sqrt(G), size=size )
-            # c_rn   = self.r + np.log(1 - self.nu*self.mu - self.nu*self.sigma**2/2) / self.nu        
-            c_hist = self.c
-        # c = c_rn  #-> if we want a risk-neutral 'c'
-        c = c_hist
-        Inc = np.array(c).reshape(-1,1) * self.dt + Y
-        n_u = self.n_underlyings
-        self.next_states[1:n_u+1, :] = self.current_state[1:n_u+1].reshape(-1,1) * np.exp( Inc )
-        '''
--- scenarioTree.py
+++ scenarioTree.py
@@ -49,7 +49,7 @@ class ScenarioTree(nx.DiGraph):
             # For each node of the last generated period add its children through the StochModel class
             for parent_node in last_added_nodes:
                 # Probabilities and prices are given by the stochastic model chosen (MM or GBM)
-                p, x = self._generate_one_time_step(self.branching_factors[i], self._node[parent_node]['prices']) # TODO:ALE E IONNY HANNO AGGIUNTO UN INPUT: depth-i
+                p, x = self._generate_one_time_step(self.branching_factors[i], self._node[parent_node]['prices'], depth-i) # TODO: added input depth-i (corresponds to remaining_times)
                 # Add all the generated nodes to the tree
                 for j in range(self.branching_factors[i]):
                     id_new_node = count
@@ -175,11 +175,12 @@ class ScenarioTree(nx.DiGraph):
         else:
             plt.show()
 
-    def _generate_one_time_step(self, n_scenarios, parent_node): #TODO ALE E IONNY HANNO AGGIUNTO UN INPUT: remaining_times
+    def _generate_one_time_step(self, n_scenarios, parent_node, remaining_times): #TODO: added input remaining_times
         # Given a parent node and the number of children to generate, it returns the children with corresponding probabilities
         prob, prices = self.stoch_model.simulate_one_time_step(
             n_scenarios,
-            parent_node #TODO ALE E IONNY HANNO AGGIUNTO QUESTO INPUT: remaining_times
+            parent_node,
+            remaining_times # TODO: added input remaining_times
         )
         return prob, prices
     
