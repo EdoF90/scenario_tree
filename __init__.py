421dfdadc0f7da3129dc5993f0a76fdf9ba65cd8
AlessandroOrame <s315025@studenti.polito.it>
2024-10-28 16:28:16 +0100

    Added MomentMatchingForHedging as stochastic model

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
 from .scenarioTree import ScenarioTree
 from .momentMatching import MomentMatching
 from .brownianMotionForHedging import BrownianMotionForHedging
+from .momentMatchingForHedging import MomentMatchingForHedging
 
 
 __all__ = [
     "ScenarioTree",
     "MomentMatching",
-    "BrownianMotionForHedging"
+    "BrownianMotionForHedging",
+    "MomentMatchingForHedging"
 ]
