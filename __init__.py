d83e01fac880e6ea9db1591b144ed16557bed1d6
ioanareut <s316671@studenti.polito.it>
2024-11-23 18:06:26 +0100

    Added BrownianMotionForHedging with E[XY] optimized with Gurobi.

--- __init__.py
+++ __init__.py
@@ -2,11 +2,13 @@ from .scenarioTree import ScenarioTree
 from .momentMatching import MomentMatching
 from .brownianMotionForHedging import BrownianMotionForHedging
 from .momentMatchingForHedging import MomentMatchingForHedging
+from .brownianMotionForHedging_Gurobi import BrownianMotionForHedging_Gurobi
 
 
 __all__ = [
     "ScenarioTree",
     "MomentMatching",
     "BrownianMotionForHedging",
-    "MomentMatchingForHedging"
+    "MomentMatchingForHedging",
+    "BrownianMotionForHedging_Gurobi"
 ]
